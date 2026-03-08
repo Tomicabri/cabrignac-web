211e157510e17dac45d20d4e5df4e47cd66317c4
tomicabri <tomas.cabrignac@gmail.com>
2026-03-08 11:43:34 -0300

    Agrego favicon para cambiar el icono

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter, Playfair_Display } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-// comentario de prueba
+
 const _inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const _playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
 
@@ -25,21 +25,7 @@ export const metadata: Metadata = {
   authors: [{ name: 'Tomás Cabrignac' }],
 
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/logo/favicon2.png',
   },
 }
 
@@ -53,9 +39,9 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="es" className={`${_inter.variable} ${_playfair.variable}`}>
       <body className="font-sans antialiased">
@@ -64,5 +50,4 @@ export default function RootLayout({
       </body>
     </html>
   )
-}
-
+} 
